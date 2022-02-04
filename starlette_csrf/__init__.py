7a14a8eab3716f70f8633fec72867ea42e4d314d
François Voron <fvoron@gmail.com>
2022-02-04 14:13:18 +0100

    Bump version: 1.4.0 → 1.4.1

--- starlette_csrf/__init__.py
+++ starlette_csrf/__init__.py
@@ -1,5 +1,5 @@
 """Starlette middleware implementing Double Submit Cookie technique to mitigate CSRF."""
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 
 from starlette_csrf.middleware import CSRFMiddleware  # noqa: F401
