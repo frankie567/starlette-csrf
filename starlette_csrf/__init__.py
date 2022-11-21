0a8c423c1c1f6aec9c57d6f86f3b196ac90ab28e
François Voron <fvoron@gmail.com>
2022-11-21 09:50:23 +0100

    Bump version 1.4.4 → 1.4.5
    Improvements
    ------------
    
    * Bump `starlette >=0.14.2`
    * Use Hatch for package management

--- starlette_csrf/__init__.py
+++ starlette_csrf/__init__.py
@@ -1,5 +1,5 @@
 """Starlette middleware implementing Double Submit Cookie technique to mitigate CSRF."""
 
-__version__ = "1.4.4"
+__version__ = "1.4.5"
 
 from starlette_csrf.middleware import CSRFMiddleware  # noqa: F401
