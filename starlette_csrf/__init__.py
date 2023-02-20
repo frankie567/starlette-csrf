3b42fd66f8304a5721877fcd4285e1a8c2f6c338
François Voron <fvoron@gmail.com>
2023-02-20 15:51:25 +0100

    Bump version 1.4.5 → 2.0.0
    Breaking changes
    ----------------
    
    * Initializer arguments other than `app` and `secret` are now keyword-only.
        * This shouldn't affect you if you add the middleware normally using Starlette's `Middleware` or FastAPI's `.add_middleware`.
    
    New features
    ------------
    
    * Add a `required_urls` argument to set route patterns where CSRF should **always** be enforced, no matter the method or cookies present.
        * Useful to prevent attacks like [Login CSRF](https://cheatsheetseries.owasp.org/cheatsheets/Cross-Site_Request_Forgery_Prevention_Cheat_Sheet.html#login-csrf).
        * Thanks @bkis for the idea and feedback

--- starlette_csrf/__init__.py
+++ starlette_csrf/__init__.py
@@ -1,5 +1,5 @@
 """Starlette middleware implementing Double Submit Cookie technique to mitigate CSRF."""
 
-__version__ = "1.4.5"
+__version__ = "2.0.0"
 
 from starlette_csrf.middleware import CSRFMiddleware  # noqa: F401
